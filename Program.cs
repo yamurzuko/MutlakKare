17d7f12f3e9fce7836824cb24225aab183e5dc18
yamurzuko <yamurzcan@gmail.com>
2022-10-16 12:43:42 +0300

    Complated

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MutlakKare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Satı giriniz: ");
+            string number  = Console.ReadLine();
+            Calculate(number);
+
+        }
+
+        public static void Calculate(string value)
+        {
+            string[] numbers = value.Split(" ");
+            int number;
+            int smallSum = 0;
+            int bigSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                number = Convert.ToInt32(numbers[i]);
+
+                if(number <= 67)
+                {
+                    smallSum += (67 - number);
+                }
+                else
+                {
+                    bigSum += ((number - 67) * (number - 67));
+                }
+            }
+            Console.Write("{0} ",smallSum);
+            Console.Write("{0} ",bigSum);
+
+        }
+    }
+}
